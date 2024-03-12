6f6b1bb13af1bd0e75f96618a4b5313f19db8283
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2024-03-12 14:03:21 +0800

    Basic Input argument

--- main.go
+++ main.go
@@ -24,4 +24,38 @@ func main() {
 	// Output with format
 	fmt.Printf("My name is: %s \n", "Herly Riyanto Hidayat")
 	fmt.Printf("My age is: %d tahun\n", 21,)
+
+	// Input with scan
+	var name string
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Println("Hello, ", name) // Output from input user
+
+	// Multiple input 
+	var nama, address string
+    fmt.Print("Enter your name and address : ")
+    fmt.Scan(&nama, &address) // set input to variable 'name' and 'address'
+
+    fmt.Println("Hello", nama)
+    fmt.Println("Your address", address)
+
+	// Scanln
+	var yourName, yourAddress string
+    fmt.Print("Enter your name : ")
+    fmt.Scanln(&yourName)
+	fmt.Print("\n")
+    fmt.Print("Enter your address : ")
+    fmt.Scanln(&yourAddress)
+
+    fmt.Println("Hello", yourName)
+    fmt.Println("Your address", yourAddress)
+
+	// Scanf
+	var namamu, alamatmu string
+
+    fmt.Print("Enter your name and address : ")
+    fmt.Scanf("%s %s", &namamu, &alamatmu) // set input with format string to variable name with multiple input
+
+    fmt.Println("Hello", namamu)
+    fmt.Println("Your address", alamatmu)
 }
