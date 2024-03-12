257da6dce67100285112f257d528e6651d908403
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2024-03-12 13:51:03 +0800

    Basic output

--- main.go
+++ main.go
@@ -14,4 +14,14 @@ func main() {
     */
 
 	fmt.Println("Hello Go!")
+	fmt.Println("Herly", 21, true) // Multiple output
+
+	// Output without enter after
+	fmt.Print("This is output Print ")
+    fmt.Print("Hello Go!")
+	fmt.Print("This is output Print\n") // \n meaning enter or add new line after this line
+
+	// Output with format
+	fmt.Printf("My name is: %s \n", "Herly Riyanto Hidayat")
+	fmt.Printf("My age is: %d tahun\n", 21,)
 }
